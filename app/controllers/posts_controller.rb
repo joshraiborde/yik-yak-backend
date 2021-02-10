788ffba61cfcd5386dcaccea21db6dea739b2392
joshsrai <joshsrai@gmail.com>
2021-02-10 17:00:49 -0500

    created yik-yak/yik-yak-backend/app/controllers/posts_controller.rb

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+    def index
+        render json: Post.all
+    end
+    
+    def create
+         post = Post.new(post_params)
+    
+         if post.save
+            render json: post
+         end
+
+    end
+
+
+    private
+
+    def post_params
+        params.require(:post).permint(:content)
+    end
+end
