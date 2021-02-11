efca651d31ffa4f87d4e175e37c0cd8428775b65
joshsrai <joshsrai@gmail.com>
2021-02-11 14:17:43 -0500

    spell check

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,6 @@ class PostsController < ApplicationController
     private
 
     def post_params
-        params.require(:post).permint(:content)
+        params.require(:post).permit(:content)
     end
 end
