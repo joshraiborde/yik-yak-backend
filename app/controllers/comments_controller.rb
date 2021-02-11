7240271cdd5d4ed2056d8ff74cff464667c56e5f
joshsrai <joshsrai@gmail.com>
2021-02-11 14:19:30 -0500

    added index create and private comment params for the comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,24 @@
 class CommentsController < ApplicationController
+    # 2021-02-11 01 added
+    def index
+        render json: Comment.all
+    end
+
+    def create
+        byebug
+        comment = Comment.new(comment_params)
+
+        if comment.save
+            render json: comment
+        else
+            byebug
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content)
+    end
+
 end
