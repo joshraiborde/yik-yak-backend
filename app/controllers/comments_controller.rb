df6f820496b9318307b1e79d6ff9f4f6380c3246
joshsrai <joshsrai@gmail.com>
2021-02-12 10:33:20 -0500

    commented out things

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,20 +5,24 @@ class CommentsController < ApplicationController
     end
 
     def create
-        # byebug
         comment = Comment.new(comment_params)
-
         if comment.save
             render json: comment
-        else
-            byebug
         end
     end
+
+    # def destroy
+    #     comment.destroy
+    # end
 
     private
 
     def comment_params
         params.require(:comment).permit(:content, :post_id)
     end
+
+    # def set_comment
+    #     comment = Comment.find(params[:id])
+    # end
 
 end
